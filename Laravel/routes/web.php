7b5c6d0bedec18b8d2bf4e90ee69d6d994ecb575
krzysztofmotas <127015975+krzysztofmotas@users.noreply.github.com>
2024-05-23 16:56:09 +0200

    Konfiguracja Oracle w Laravel

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-oracle-connection', function () {
+    try {
+        DB::connection('oracle')->getPdo();
+        return "Pomyślnie połączono z bazą danych Oracle.";
+    } catch (\Exception $e) {
+        return "Nie można połączyć się z bazą danych Oracle: " . $e->getMessage();
+    }
 });
